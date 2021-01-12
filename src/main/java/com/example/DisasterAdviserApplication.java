1f8a2f45fbe82610bb3749cc3d685b376ebd7839
Pavlo Berezin <pavlo.berezin@eleks.com>
2021-01-13 00:59:29 +0200

    Initial Google Calendar polling

--- src/main/java/com/example/DisasterAdviserApplication.java
+++ src/main/java/com/example/DisasterAdviserApplication.java
@@ -5,6 +5,7 @@ import akka.http.javadsl.ServerBinding;
 import akka.http.javadsl.server.Route;
 import akka.actor.typed.ActorSystem;
 import com.example.actors.DisasterNasaSource;
+import com.example.actors.GoogleCalendarSource;
 
 import java.net.InetSocketAddress;
 import java.util.concurrent.CompletionStage;
@@ -14,14 +15,14 @@ public class DisasterAdviserApplication {
     // #start-http-server
     static void startHttpServer(Route route, ActorSystem<?> system) {
         CompletionStage<ServerBinding> futureBinding =
-            Http.get(system).newServerAt("localhost", 8080).bind(route);
+                Http.get(system).newServerAt("localhost", 8080).bind(route);
 
         futureBinding.whenComplete((binding, exception) -> {
             if (binding != null) {
                 InetSocketAddress address = binding.localAddress();
                 system.log().info("Server online at http://{}:{}/",
-                    address.getHostString(),
-                    address.getPort());
+                        address.getHostString(),
+                        address.getPort());
             } else {
                 system.log().error("Failed to bind HTTP endpoint, terminating system", exception);
                 system.terminate();
@@ -42,8 +43,11 @@ public class DisasterAdviserApplication {
         ActorSystem<DisasterNasaSource.Command> disasterSystem =
                 ActorSystem.create(DisasterNasaSource.create(), "disaster-system");
         disasterSystem.tell(new DisasterNasaSource.ReadDisasters());
-    }
 
+        ActorSystem<GoogleCalendarSource.Command> calendarSystem =
+                ActorSystem.create(GoogleCalendarSource.create(), "calendar-system");
+        calendarSystem.tell(new GoogleCalendarSource.ReadEvents());
+    }
 }
 
 
--- src/main/java/com/example/actors/GoogleCalendarSource.java
+++ src/main/java/com/example/actors/GoogleCalendarSource.java
@@ -1,26 +1,120 @@
 package com.example.actors;
 
+import akka.Done;
+import akka.actor.typed.ActorSystem;
 import akka.actor.typed.Behavior;
+import akka.actor.typed.PostStop;
 import akka.actor.typed.javadsl.AbstractBehavior;
 import akka.actor.typed.javadsl.ActorContext;
 import akka.actor.typed.javadsl.Behaviors;
 import akka.actor.typed.javadsl.Receive;
+import akka.http.javadsl.Http;
+import akka.http.javadsl.marshallers.jackson.Jackson;
+import akka.http.javadsl.model.HttpEntity;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.unmarshalling.Unmarshaller;
+import akka.stream.javadsl.Sink;
+import akka.stream.javadsl.Source;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
 
 public class GoogleCalendarSource extends AbstractBehavior<GoogleCalendarSource.Command> {
 
-    public interface Command{}
+    public interface Command {
+    }
 
-    static Behavior<Command> create() {
+    public static final class ReadEvents implements Command {
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class GoogleCalendarEvent {
+        public GoogleCalendarEvent() {
+        }
+
+        public String kind;
+        public String etag;
+        public String summary;
+        public String nextSyncToken;
+        public List<GoogleCalendarEventItem> items;
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class GoogleCalendarEventItem {
+        public GoogleCalendarEventItem() {
+        }
+
+        public String id;
+        public String location;
+        public String status;
+        public String htmlLink;
+    }
+
+
+    public static final String GOOGLE_API = "https://www.googleapis.com";
+    public static final String key = "AIzaSyCKYFpTRY-IhXNrsgjRvHwbICk2F3fOt1k";
+    final Http http = Http.get(getContext().getSystem());
+
+    public static Behavior<Command> create() {
         return Behaviors.setup(GoogleCalendarSource::new);
     }
 
     private GoogleCalendarSource(ActorContext<GoogleCalendarSource.Command> context) {
         super(context);
+        getContext().getLog().info("GoogleCalendarSource created");
     }
 
     // TODO: Get Google Events here and send to Analyzer if there some disasters on event places
     @Override
     public Receive<Command> createReceive() {
-        return null;
+        getContext().getLog().info("GoogleCalendarSource createReceive");
+        return newReceiveBuilder()
+                .onMessage(ReadEvents.class, this::onReadEvents)
+                .onSignal(PostStop.class, signal -> onPostStop())
+                .build();
     }
+
+    private Behavior<GoogleCalendarSource.Command> onReadEvents(ReadEvents readEvents) {
+        getContext().getLog().info("GoogleCalendarSource onReadDisasters");
+        String id = "4qljo7l1qoo4km733ifm92vbog@group.calendar.google.com";
+        ActorSystem<Void> system = getContext().getSystem();
+
+        Unmarshaller<HttpEntity, GoogleCalendarEvent> unmarshaller = Jackson.unmarshaller(GoogleCalendarEvent.class);
+        AtomicReference<String> nextSyncToken = new AtomicReference<>("");
+
+        Source.tick(Duration.ofSeconds(1), Duration.ofSeconds(1), nextSyncToken)
+                .mapAsync(1, token -> {
+                    String url = GOOGLE_API + "/calendar/v3/calendars/" + id + "/events?key=" + key;
+
+                    if (token.get().length() != 0) {
+                        url += "&syncToken=" + token.get();
+                    }
+
+                    HttpRequest httpRequest = HttpRequest.GET(url);
+
+                    return http.singleRequest(httpRequest);
+                })
+                .mapAsync(1, r -> unmarshaller.unmarshal(r.entity(), system))
+                .wireTap(r -> {
+                    nextSyncToken.set(r.nextSyncToken);
+                })
+//              .map(r -> r.items.stream().filter(item -> !item.status.equals("cancelled")).collect(Collectors.toList()))
+                .map(r -> r.items.stream().filter(item -> item.location != null && item.status.equals("confirmed")).collect(Collectors.toList()))
+                .filter(items -> items.size() > 0)
+                .runWith(Sink.foreach(response -> {
+                    response.forEach(item -> System.out.println(item.id + " " + item.location));
+                }), system);
+
+        return this;
+    }
+
+    private GoogleCalendarSource onPostStop() {
+        getContext().getLog().info("DisasterNasaSourceActor stopped");
+        return this;
+    }
+
 }
